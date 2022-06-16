dd74e453f553d0dd7923fc3908f62acbb1ee1983
egrove <ellen.grove95@gmail.com>
2022-06-15 23:23:41 -0500

    added additional base layer and began legend work

--- Leaflet-Part-1/js/script.js
+++ Leaflet-Part-1/js/script.js
@@ -3,15 +3,21 @@ function createMap(markerLayer) {
 
     var map = L.map('map').setView([39.8283,-98.5795], 4);
 
-    var base = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '© OpenStreetMap'
+    });
+
+    var USGS_USImagery = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}', {
+        maxZoom: 20,
+        attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
     });
 
     var earthquakeLayer = L.layerGroup(markerLayer);
 
     var baseLayers = {
-        "OpenStreetMap": base
+        "OpenStreetMap" : osm,
+        "U.S. Geogological Survey" : USGS_USImagery
     };
     
     var overlays = {
@@ -19,6 +25,27 @@ function createMap(markerLayer) {
     };
     
     L.control.layers(baseLayers, overlays).addTo(map);
+
+    // Set up the legend.
+    var legend = L.control({ position: "bottomright" });
+
+    legend.onAdd = function(map){
+        var div=L.DomUtil.create('div','legend');
+        var labels=["-10-10","10-30",
+                    "30-50","50-70","70-90","90+"];
+        var grades = [9,29,49,69,89,91];
+        div.innerHTML=`<div><b>Depth</b></div`;
+        for (var i = 0; i < grades.length; i++){
+            div.innerHTML+=`<li> <span style="background-color: ${pinColor(grades[i])}">test</span> ${labels[i]}</li>`; 
+        }
+        console.log(div);
+        return div;
+    }
+    legend.addTo(map);
+    // console.log(legend);
+
+  // Adding the legend to the map
+//   legend.addTo(myMap);
   
 }
 
